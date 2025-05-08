666470f2f76bc333002a0a2f2c72aed9a7307470
cecen <slhtk@gmail.com>
2025-05-08 23:20:38 +0300

    second line added

---
+++ SquareTurtle.py
@@ -0,0 +1,37 @@
+import turtle
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("yellow")
+drawing_board.title("Kare Cizimi")
+turtle_instance = turtle.Turtle()
+
+turtle_instance.forward(100)
+turtle_instance.right(90)
+turtle_instance.forward(100)
+turtle_instance.right(90)
+turtle_instance.forward(100)
+turtle_instance.right(90)
+turtle_instance.forward(100)
+turtle_instance.right(90)
+
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+turtle_instance.left(90)
+
+turtle_instance.forward(300)
+turtle_instance.right(90)
+turtle_instance.forward(300)
+turtle_instance.right(90)
+turtle_instance.forward(300)
+turtle_instance.right(90)
+turtle_instance.forward(300)
+turtle_instance.right(90)
+
+
+turtle.done()
+
