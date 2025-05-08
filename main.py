58da03c49e33f1ae7fbbf32dd65a4c4b20d15833
cecen <slhtk@gmail.com>
2025-05-08 22:30:44 +0300

    first line added

---
+++ main.py
@@ -0,0 +1,23 @@
+import turtle
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("yellow")
+drawing_board.title("Pyhton turtle")
+turtle_instance = turtle.Turtle()
+
+turtle_instance.forward(400)
+turtle_instance.right(144)
+turtle_instance.forward(400)
+turtle_instance.right(144)
+turtle_instance.forward(400)
+turtle_instance.right(144)
+turtle_instance.forward(400)
+turtle_instance.right(144)
+turtle_instance.forward(400)
+turtle_instance.right(144)
+
+
+
+
+
+turtle.done()
